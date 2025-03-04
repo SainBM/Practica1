7cef8e3d1d4ae7de9777d0a455d853ca1a3c91e5
SainBM <zs23014005@estudiantes.uv.mx>
2025-03-03 22:31:07 -0600

    Update Ejemplo 1 - F01.py

--- F01/Ejemplo 1 - F01.py
+++ F01/Ejemplo 1 - F01.py
@@ -1,17 +1,23 @@
 # Hola esto es una prueba
 
+
 # Hola esto lo estoy escribiendo desde mi PC
-numero1 = 5
-numero2 = 3
+#Team CSGO
+#Este codigo define dos variables numericas y realiza cuatro operaciones, suma, resta, multiplicación y división, imprimiendo el resultado de estas cuatro operaciones.
+# 2025/ 03/ 03 - V. 1. 0. 1.
+#Trabajo: SainBM 
+
+numero1 = 5 # Se define la variable uno, en este caso un numero
+numero2 = 3 # Se define la variable dos 
 
 
-suma = numero1 + numero2
-resta = numero1 - numero2
-multiplicacion = numero1 * numero2
-division = numero1 / numero2
+suma = numero1 + numero2 # Se define y realiza la suma con las dos variables y +
+resta = numero1 - numero2 # Se define y reliza la resta -
+multiplicacion = numero1 * numero2 # Se define y realiza la multiplicacion con *
+division = numero1 / numero2 # Se define y realiza la división con /
 
 
-print("Suma:", suma)
-print("Resta:", resta)
+print("Suma:", suma) # Print() imprime el resultado del parentesis, "Suma:" es un indicador de que operación se realiza y suma es el resultado de la operación
+print("Resta:", resta) # Hace lo mismo que la linea anterior asi como la linea 22 y 23
 print("Multiplicación:", multiplicacion)
 print("División:", division)
